c0751b25157c25b2e4ec2abb64b962361aae0621
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-17 22:01:57 +0530

    [Ajay]-LineComparison

---
+++ LineComparisonProblem/LineEqualityCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LineEqualityCheck
+    {
+        public static void Equals(double X3, double X4, double Y4, double X5, double X6, double Y5, double Y6, double y6)
+        {
+            double temp1 = Math.Pow((X4 - X3), 2);
+            double temp2 = Math.Pow((Y4 - Y4), 2);
+            double length = Math.Sqrt((temp1 + temp2));
+            double temp3 = Math.Pow((X6 - X5), 2);
+            double temp4 = Math.Pow((Y6 - Y5), 2);
+            double length2 = Math.Sqrt((temp3 + temp4));
+            Console.WriteLine("Length1.Equals(Length2)");
+
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
-            Console.WriteLine("1:Distance between line \n2:Line equality check \n3:Comparison between line");
+            Console.WriteLine("1:Distance between line \n2:Line equality check ");
             int option = Convert.ToInt32(Console.ReadLine());
             //users switch case to call differnt method
             switch (option)
@@ -22,6 +22,26 @@
                     var finalResult = DistanceBetweenLine.Distance(X1, X2, Y1, Y2);
 
                     Console.WriteLine("Distance between co-ordinates {0},{1},{2},{3},{4}", X1, X2, Y1, Y2, finalResult);
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter X3 & Y3 Co-ordinates");
+                    var X3 = Convert.ToDouble(Console.ReadLine());
+                    var Y3 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X4 & Y4 Co-ordinates");
+                    var X4 = Convert.ToDouble(Console.ReadLine());
+                    var Y4 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X5 & Y5 Co-ordinates");
+                    var X5 = Convert.ToDouble(Console.ReadLine());
+                    var Y5 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X6 & Y6 Co-ordinates");
+                    var X6 = Convert.ToDouble(Console.ReadLine());
+                    var Y6 = Convert.ToDouble(Console.ReadLine());
+
+                    LineEqualityCheck.Equals(X3, Y3, X4, Y4, X5, Y5, X6, Y6);
+
                     break;
             }
         }
