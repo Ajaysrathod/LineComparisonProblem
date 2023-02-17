45d93b281eede08b3585286757899429f35103d9
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-02-17 22:11:06 +0530

    [Ajay]-LineComparison

---
+++ LineComparisonProblem/ComparisonBetweenLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class ComparisonBetweenLine
+    {
+        public static void CompareOfLine(double X7, double X8, double X9, double X10, double Y7, double Y8, double Y9, double Y10)
+        {
+            double temp1 = Math.Pow((X8 - X7), 2);
+            double temp2 = Math.Pow((Y8 - Y7), 2);
+            double LengthOfFirstLine = Math.Sqrt(temp1 + temp2);
+            double temp3 = Math.Pow((X10 - X9), 2);
+            double temp4 = Math.Pow((Y10 - Y9), 2);
+            double LengthOfSecondLine = Math.Sqrt(temp3 + temp4);
+
+            double result = LengthOfFirstLine.CompareTo(LengthOfSecondLine);
+            if (result == 0)
+            {
+                Console.WriteLine("Length of two lines are equal");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine("Length of line1 is greater than line2");
+            }
+            else
+            {
+                Console.WriteLine("Length of line1 is less than line2");
+            }
+        }
+
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
-            Console.WriteLine("1:Distance between line \n2:Line equality check ");
+            Console.WriteLine("1:Distance between line \n2:Line equality check \n3:Comparison Between Line");
             int option = Convert.ToInt32(Console.ReadLine());
             //users switch case to call differnt method
             switch (option)
@@ -43,7 +43,30 @@
                     LineEqualityCheck.Equals(X3, Y3, X4, Y4, X5, Y5, X6, Y6);
 
                     break;
+                case 3:
+                    Console.WriteLine("Please enter X7 & Y7 Co-ordinates");
+                    var X7 = Convert.ToDouble(Console.ReadLine());
+                    var Y7 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X8 & Y8 C0-ordinates");
+                    var X8 = Convert.ToDouble(Console.ReadLine());
+                    var Y8 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X9 & Y9 Co-ordinates");
+                    var X9 = Convert.ToDouble(Console.ReadLine());
+                    var Y9 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X10 & Y10 Co-ordinates");
+                    var X10 = Convert.ToDouble(Console.ReadLine());
+                    var Y10 = Convert.ToDouble(Console.ReadLine());
+                    ComparisonBetweenLine.CompareOfLine(X7, Y7, X8, Y8, X9, Y9, X10, Y10);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
             }
+
+        }
         }
     }
-}
+
